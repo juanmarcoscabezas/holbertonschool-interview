deb95619c8557e337739d571e95088eb2e000663
juanmarcos <juanmarcoscabezas@gmail.com>
2020-07-19 17:32:21 -0500

    Getting only the correct keys

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -6,16 +6,12 @@ def canUnlockAll(boxes):
         return False
     if len(boxes) == 0:
         return False
-    keys = boxes[0]
-    keys.insert(0, 0)
-    return go_to_box(keys, boxes)
-
-
-def go_to_box(keys, boxes):
+    keys = []
+    keys.append(0)
     for key in keys:
         new_keys = boxes[key]
         for new_key in new_keys:
-            if new_key not in keys:
+            if new_key not in keys and new_key < len(boxes):
                 keys.append(new_key)
     if len(keys) == len(boxes):
         return True
