c1d3b545774daae79e65a00769b09a6c1f446814
juanmarcos <juanmarcoscabezas@gmail.com>
2020-07-19 17:13:52 -0500

    Test 0

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -1,4 +1,18 @@
 #!/usr/bin/python3
 
+
 def canUnlockAll(boxes):
-    pass
+    keys = boxes[0]
+    keys.insert(0, 0)
+    return go_to_box(keys, boxes)
+
+
+def go_to_box(keys, boxes):
+    for key in keys:
+        new_keys = boxes[key]
+        for new_key in new_keys:
+            if new_key not in keys:
+                keys.append(new_key)
+    if len(keys) == len(boxes):
+        return True
+    return False
