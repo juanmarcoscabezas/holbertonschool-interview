da93878d8ac7f91b4c9554bce5a7ab982d6ee7fe
Juan Marcos <juanmarcoscabezas@gmail.com>
2020-08-10 13:24:37 -0500

    Adding comments

--- 0x03-minimum_operations/0-main.py
+++ 0x03-minimum_operations/0-main.py
@@ -5,7 +5,7 @@ Main file for testing
 
 minOperations = __import__('0-minoperations').minOperations
 
-n = 3
+n = 4
 print("Min number of operations to reach {} characters: {}".format(n, minOperations(n)))
 
 n = 12
--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -1,23 +1,35 @@
 #!/usr/bin/python3
+"""
+Script for minOperations problem
+"""
 
 
 def minOperations(n):
+    """
+    Finds the min operations
+    """
     if n <= 0:
         return 0
     my_str = 'H'
     copy = ''
     iterations = 0
     while len(my_str) < n:
-        copy = copy_str(my_str, copy)
+        copy = copy_str(my_str)
         my_str = paste_str(my_str, copy)
-        iterations += 1
+        iterations += 2
+        print(my_str)
     return iterations
 
 
 def paste_str(my_str, copy):
+    """
+    Makes the paste
+    """
     return my_str + copy
 
 
-def copy_str(my_str, copy):
-    copy = my_str
-    return copy
+def copy_str(my_str):
+    """
+    Makes the copy
+    """
+    return my_str
