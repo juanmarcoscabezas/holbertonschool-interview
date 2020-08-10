e4dd583f0b59071ad68a8c2f928ec991df159530
Juan Marcos <juanmarcoscabezas@gmail.com>
2020-08-10 13:48:17 -0500

    Test 1

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -15,22 +15,8 @@ def minOperations(n):
     iterations = 0
     while len(my_str) < n:
         if n % len(my_str) == 0:
-            copy = copy_str(my_str)
+            copy = my_str[:]
             iterations += 1
-        my_str = paste_str(my_str, copy)
+        my_str = my_str + copy
         iterations += 1
     return iterations
-
-
-def paste_str(my_str, copy):
-    """
-    Makes the paste
-    """
-    return my_str + copy
-
-
-def copy_str(my_str):
-    """
-    Makes the copy
-    """
-    return my_str
