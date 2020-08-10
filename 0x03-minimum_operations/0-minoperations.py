5f60fe41c3b899103f5b746df3e0d2c948f8f885
Juan Marcos <juanmarcoscabezas@gmail.com>
2020-08-10 12:35:20 -0500

    NAive solution

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -1,4 +1,22 @@
 #!/usr/bin/python3
 
+
 def minOperations(n):
-    return 0
+    my_str = 'H'
+    copy = ''
+    iterations = 0
+    while len(my_str) < n:
+        copy = copy_str(my_str, copy)
+        my_str = paste_str(my_str, copy)
+        iterations += 1
+        print(my_str)
+    return iterations
+
+
+def paste_str(my_str, copy):
+    return my_str + copy
+
+
+def copy_str(my_str, copy):
+    copy = my_str
+    return copy
