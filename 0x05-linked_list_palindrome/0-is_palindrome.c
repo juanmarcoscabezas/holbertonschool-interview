742438cf92f5303e75638769da2f05c24b454068
Juan Marcos <juanmarcoscabezas@gmail.com>
2020-08-23 17:18:14 -0500

    First try

--- 0x05-linked_list_palindrome/0-is_palindrome.c
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -1,6 +1,20 @@
 #include "lists.h"
 
+/**
+ * is_palindrome - Finds if a linked list is a palindrome
+ * @head: Head of the Linked list
+ * Return: 0 if it is nota palidrome, 1 otherwise
+ */
 int is_palindrome(listint_t **head)
 {
+	listint_t *iterator = *head;
 
+	if (!iterator)
+		return (1);
+
+	while (iterator)
+	{
+		iterator = iterator->next;
+	}
+	return (1);
 }
