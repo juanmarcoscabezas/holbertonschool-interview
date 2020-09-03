f4ac022189602c4ec79a48424189a0fab33f9dc8
Juan Marcos <juanmarcoscabezas@gmail.com>
2020-09-02 20:38:29 -0500

    test3

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -10,7 +10,7 @@ Input format: <IP Address> - [<date>] "GET /projects/260 HTTP/1.1"
 import sys
 
 
-total_file_size = 0
+total_file_size = {'size': 0}
 codes = {
     '200': 0,
     '301': 0,
@@ -21,28 +21,43 @@ codes = {
     '405': 0,
     '500': 0
 }
-num_lines = 0
 
 
 def print_data():
-    print('File size: {}'.format(total_file_size))
+    """
+        Prints the resume
+    """
+    print('File size: {}'.format(total_file_size['size']))
     for key in sorted(codes.keys()):
         if codes[key] > 0:
             print('{}: {}'.format(key, codes[key]))
     return 0
 
+
+def operate_resume(line):
+    """
+        Operates with the resume stats
+    """
+    try:
+        line = line.split(' ')
+        size = line[-1]
+        total_file_size['size'] += int(size)
+        if line[-2] in codes:
+            codes[line[-2]] += 1
+    except Exception as e:
+        print(e)
+        pass
+
+
 if __name__ == '__main__':
+    num_lines = 0
     try:
         for line in sys.stdin:
-            line = line.split(' ')
-            size = line[-1]
-            total_file_size += int(size)
-
-            codes[line[-2]] += 1
+            operate_resume(line)
             if num_lines == 9:
                 num_lines = print_data()
             else:
                 num_lines += 1
-    except (KeyboardInterrupt, EOFError):
+    except KeyboardInterrupt:
         print_data()
         exit()
