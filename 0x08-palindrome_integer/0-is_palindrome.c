016d87d7bb749448701f72ada1e34bb5f46325ff
Juan Marcos <juanmarcoscabezas@gmail.com>
2020-09-15 17:52:46 -0500

    Starting project

---
+++ 0x08-palindrome_integer/0-is_palindrome.c
@@ -0,0 +1,13 @@
+#include "palindrome.h"
+
+/**
+ * is_palindrome - Finds if a number is palindrome
+ * @n: Number to find
+ * Return: 1 if @n is palindrome, 0 otherwise
+ */
+int is_palindrome(unsigned long n)
+{
+	if (n < 10)
+		return (1);
+	return (1);
+}
---
+++ 0x08-palindrome_integer/palindrome.h
@@ -0,0 +1,8 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdlib.h>
+
+int is_palindrome(unsigned long n);
+
+#endif /* LISTS_H */
