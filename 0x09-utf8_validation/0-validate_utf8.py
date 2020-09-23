648a9ad5e638597ea254a885cbe9b7c4c87bb30a
Juan Marcos <juanmarcoscabezas@gmail.com>
2020-09-23 16:25:04 -0500

    test9

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -5,9 +5,22 @@ Validate UTF8
 
 
 def validUTF8(data):
-    if len(data) == 0:
-        return False
-    for number in data:
-        if number > 255:
+    """Check if a set of integers are valid utf-8 format"""
+    num_bytes = 0
+    for char_d in data:
+        byte = char_d & 0xff
+        if num_bytes:
+            if (byte >> 6 == 1 or byte >> 6 == 3):
+                return False
+            num_bytes -= 1
+            continue
+
+        while 7 - num_bytes and byte & (1 << (7 - num_bytes)):
+            num_bytes += 1
+        if num_bytes == 1 or num_bytes > 4:
             return False
+        num_bytes = max(num_bytes - 1, 0)
+
+    if num_bytes:
+        return False
     return True
