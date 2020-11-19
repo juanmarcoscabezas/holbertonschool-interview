cf7601149f28f282e8b74cabb388ae7749485f06
Juan Marcos <juanmarcoscabezas@gmail.com>
2020-11-18 20:50:45 -0500

    test 1

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -4,25 +4,24 @@ const URL = 'https://swapi-api.hbtn.io/api/';
 const request = require('request');
 const idMovie = process.argv[2];
 
-async function getRequest(url) {
-    return new Promise(function(resolve, reject) {
-        request.get(url, function(err, resp, body) {
-          if (err) {
-            reject(false);
-          } else {
-            resolve(JSON.parse(body));
-          }
-        })
+async function getRequest (url) {
+  return new Promise(function (resolve, reject) {
+    request.get(url, function (err, resp, body) {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(JSON.parse(body));
+      }
     });
+  });
 }
 
-(async function main() {
-    const movie = await getRequest(URL + 'films/' + idMovie);
-    if (movie) {
-        movie.characters.forEach(async characterURL => {
-            console.log(characterURL);
-            const character = await getRequest(characterURL);
-            console.log(character.name);
-        });        
-    }
+(async function main () {
+  const movie = await getRequest(URL + 'films/' + idMovie);
+  if (typeof (movie) === 'object') {
+    movie.characters.forEach(async characterURL => {
+      const character = await getRequest(characterURL);
+      console.log(character.name);
+    });
+  }
 })();
