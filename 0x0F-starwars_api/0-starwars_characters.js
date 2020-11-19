f87ad3ccb21969a22fa421fa512d8eca34efeb41
Juan Marcos <juanmarcoscabezas@gmail.com>
2020-11-18 21:17:37 -0500

    Working with this? Endpoint limit reached

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -4,30 +4,44 @@
 Get characters from Star Wars Movie
 */
 
-const URL = 'https://swapi-api.hbtn.io/api/';
 const request = require('request');
 const idMovie = process.argv[2];
+const url = 'https://swapi-api.hbtn.io/api/films' + idMovie;
 
-async function getRequest (url) {
-  return new Promise(function (resolve, reject) {
-    request.get(url, function (err, resp, body) {
+const getName = async (ch) => {
+  return new Promise((resolve, reject) => {
+    request(ch, (err, res, body) => {
       if (err) {
         reject(err);
       } else {
-        resolve(JSON.parse(body));
+        const jsonBody = JSON.parse(body);
+        resolve(jsonBody.name);
       }
     });
   });
-}
+};
+
+const getMovie = async () => {
+  return new Promise((resolve, reject) => {
+    request(url, (err, res, body) => {
+      if (err) {
+        reject(err);
+      } else {
+        const jsonBody = JSON.parse(body);
+        resolve(jsonBody.characters);
+      }
+    });
+  });
+};
 
 (async () => {
-  return getRequest(URL + 'films/' + idMovie);
-})().then(async (movie) => {
-  if (movie.detail !== 'Not found') {
-    for (const ch of movie.characters) {
-      const character = await getRequest(ch);
-      if (character.detail === undefined) {
-        console.log(character.name);
+  return getMovie();
+})().then(async (movies) => {
+  if (typeof (movies) === 'object') {
+    for (const ch of movies) {
+      const name = await getName(ch);
+      if (typeof (movies) === 'object') {
+        console.log(name);
       }
     }
   }
