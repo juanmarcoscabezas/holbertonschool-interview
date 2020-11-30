8a862e3c63a617eeb20572335e4ad1732e027451
Juan Marcos <juanmarcoscabezas@gmail.com>
2020-11-30 09:38:37 -0500

    Hope this works

--- 0x11-heap_sort/0-heap_sort.c
+++ 0x11-heap_sort/0-heap_sort.c
@@ -1,49 +1,69 @@
 #include "sort.h"
 
-void heapify(int *array, int size, int iterator);
-void swap(int *a, int *b);
+void heapify(int *array, int size, int iterator, int real_size);
+void swap(int *a, int *b, int *array, int size);
 
+/**
+ * heap_sort - Heap sort algorithm
+ * @array: Array to print
+ * @size: Array size
+ * Return:
+ */
 void heap_sort(int *array, size_t size)
 {
 	int iterator;
 
 	for (iterator = (size / 2) - 1; iterator >= 0; iterator--)
 	{
-		heapify(array, size, iterator);
-		print_array(array, size);
+		heapify(array, size, iterator, size);
 	}
-		
-    
-    for (iterator = size - 1; iterator >= 0; iterator--)
+
+	for (iterator = size - 1; iterator >= 0; iterator--)
 	{
-		swap(&array[0], &array[iterator]);
-		heapify(array, iterator, 0);
-		print_array(array, size);
+		swap(&array[0], &array[iterator], array, size);
+		heapify(array, iterator, 0, size);
 	}
 }
 
-void heapify(int *array, int size, int iterator)
+/**
+ * heapify - Heapify
+ * @array: Array to print
+ * @size: Array size
+ * @iterator: Current array position
+ * @real_size: Real array size, not the split one
+ * Return:
+ */
+void heapify(int *array, int size, int iterator, int real_size)
 {
-    int max = iterator;
-    int left_child = (2 * iterator) + 1;
-    int rigth_child = (2 * iterator) + 2;
+	int max = iterator;
+	int left_child = (2 * iterator) + 1;
+	int rigth_child = (2 * iterator) + 2;
 
-    if (left_child < size && array[left_child] > array[max])
-        max = left_child;
-    
-    if (left_child < size && array[rigth_child] > array[max])
-        max = left_child;
-    
-    if (max != iterator)
-    {
-        swap(&array[iterator], &array[max]);
-        heapify(array, size, max);
-    }
+	if (left_child < size && array[left_child] > array[max])
+		max = left_child;
+
+	if (rigth_child < size && array[rigth_child] > array[max])
+		max = rigth_child;
+
+	if (max != iterator)
+	{
+		swap(&array[iterator], &array[max], array, real_size);
+		heapify(array, size, max, real_size);
+	}
 }
 
-void swap(int *a, int *b)
+/**
+ * swap - Swaps array values
+ * @a: First value
+ * @b: Second value
+ * @array: Array to print
+ * @size: Array size
+ * Return:
+ */
+void swap(int *a, int *b, int *array, int size)
 {
-    int tmp = *a;
-    *a = *b;
-    *b = tmp;
+	int tmp = *a;
+	*a = *b;
+	*b = tmp;
+	print_array(array, size);
 }
