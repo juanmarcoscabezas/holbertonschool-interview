df238a597f0d2737cde23df5f76cbcc6d2074beb
Juan Marcos <juanmarcoscabezas@gmail.com>
2020-12-07 16:04:54 -0500

    Ready to test

--- 0x12-advanced_binary_search/0-advanced_binary.c
+++ 0x12-advanced_binary_search/0-advanced_binary.c
@@ -4,19 +4,22 @@ void print_array(int *array, int start, int end);
 
 int advanced_binary(int *array, size_t size, int value)
 {
-	int first = 0, last = size;
-	int middle = (first + last - 1) / 2;
+	int first = 0, last = size - 1;
+	int middle = (first + last) / 2;
 
-	while (first <= last) {
+	if (!array)
+		return (-1);
+
+	while (first <= last)
+	{
 		print_array(array, first, last);
 		if (array[middle] < value)
 			first = middle + 1;
-		else if (array[middle] == value) {
-			return (middle + 1);
-		}
 		else
 			last = middle - 1;
-		
+		if (array[middle] == value)
+			return (middle);
+
 		middle = (first + last) / 2;
 	}
 	return (-1);
@@ -26,10 +29,12 @@ void print_array(int *array, int start, int end)
 {
 	int iterator;
 
-	printf("Searching in array:");
-	for (iterator = start; iterator < end; iterator++)
+	printf("Searching in array: ");
+	for (iterator = start; iterator <= end; iterator++)
 	{
-		printf(", %d", array[iterator]);
+		if (iterator != start)
+			printf(", ");
+		printf("%d", array[iterator]);
 	}
 	putchar('\n');
 }
