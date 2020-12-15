8b10716f25c1de25066309f33428fd841f2f4fa4
Juan Marcos <juanmarcoscabezas@gmail.com>
2020-12-15 15:02:56 -0500

    Fixing typo

--- 0x13-count_it/0-count.py
+++ 0x13-count_it/0-count.py
@@ -7,7 +7,7 @@ and prints a sorted count of given keywords
 import requests
 
 
-def get_articles(subreddit, word_list, words_dict, after=""):
+def get_articles(subreddit, word_list, words_dict, words_dict_r, after="",):
     """ Get articles """
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     url += "?limit=100&after={}".format(after)
@@ -22,25 +22,27 @@ def get_articles(subreddit, word_list, words_dict, after=""):
         title_list = article.get("data").get("title").lower().split(" ")
         for title in title_list:
             if title in words_dict:
-                words_dict[title]["count"] += 1
+                words_dict[title] += 1
     after = response.json().get("data").get("after")
     if after is None:
         sorted_w = sorted(words_dict.items(), key=lambda t: t[::-1])
         sorted_w_desc = sorted(sorted_w, key=lambda tup: tup[1], reverse=True)
         for w in sorted_w_desc:
-            if w[1]["count"] > 0:
-                print("{}: {}".format(w[0], w[1]["count"] * w[1]["mul"]))
+            if w[1] > 0:
+                print("{}: {}".format(w[0], w[1] * words_dict_r[w[0]]))
         return
-    return get_articles(subreddit, word_list, words_dict, after)
+    return get_articles(subreddit, word_list, words_dict, words_dict_r, after)
 
 
 def count_words(subreddit, word_list):
     """ Count words """
     words_dict = {}
+    words_dict_r = {}
     word_list = [word.lower() for word in word_list]
     for w in word_list:
         if w not in words_dict:
-            words_dict[w] = {"count": 0, "mul": 1}
+            words_dict_r[w] = 1
+            words_dict[w] = 0
         else:
-            words_dict[w]["mul"] += 1
-    results = get_articles(subreddit, word_list, words_dict)
+            words_dict_r[w] += 1
+    results = get_articles(subreddit, word_list, words_dict, words_dict_r)
