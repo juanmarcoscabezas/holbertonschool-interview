fdd3d4e15c1e9d4335fdef8e03b154185ff15785
Juan Marcos <juanmarcoscabezas@gmail.com>
2020-12-15 14:37:32 -0500

    Multiply by the amout of items

--- 0x13-count_it/0-count.py
+++ 0x13-count_it/0-count.py
@@ -22,20 +22,26 @@ def get_articles(subreddit, word_list, words_dict, after=""):
         title_list = article.get("data").get("title").lower().split(" ")
         for title in title_list:
             if title in words_dict:
-                words_dict[title] += 1
+                words_dict[title]["count"] += 1
     after = response.json().get("data").get("after")
     if after is None:
         sorted_w = sorted(words_dict.items(), key=lambda t: t[::-1])
         sorted_w_desc = sorted(sorted_w, key=lambda tup: tup[1], reverse=True)
         for w in sorted_w_desc:
-            if w[1] > 0:
-                print("{}: {}".format(w[0], w[1]))
+            if w[1]["count"] > 0:
+                print("{}: {}".format(w[0], w[1]["count"] * w[1]["mul"]))
         return
     return get_articles(subreddit, word_list, words_dict, after)
 
 
 def count_words(subreddit, word_list):
     """ Count words """
+    words_dict = {}
     word_list = [word.lower() for word in word_list]
-    words_dict = dict.fromkeys(word_list, 0)
-    results = get_articles(subreddit, word_list, words_dict)
+    for w in word_list:
+        if w not in words_dict:
+            words_dict[w] = {"count": 0, "mul": 1}
+        else:
+            words_dict[w]["mul"] += 1
+    # words_dict = dict.fromkeys(word_list, 0)
+    get_articles(subreddit, word_list, words_dict)
