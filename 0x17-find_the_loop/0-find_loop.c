51754788b99659a89fca2f76043ac22a9af6e379
Juan Marcos <juanmarcoscabezas@gmail.com>
2021-01-26 10:05:47 -0500

    Testing on sandbox2

--- 0x17-find_the_loop/0-find_loop.c
+++ 0x17-find_the_loop/0-find_loop.c
@@ -2,5 +2,27 @@
 
 listint_t *find_listint_loop(listint_t *head)
 {
+	listint_t *har, *tor;
 
+	tor = head;
+	har = head;
+	
+	while (tor && har && har->next)
+	{
+		tor = tor->next;
+		har = har->next->next;
+
+		if (tor == har)
+		{
+			tor = head;
+			while (tor && har)
+			{
+				if (tor == har)
+					return (tor);
+				tor = tor->next;
+				har = har->next;
+			}
+		}
+	}
+	return (NULL);
 }
