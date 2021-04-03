5f4685e14ed2ae4898d7d5f5c1c0599b4567eac0
Juan Marcos <juanmarcoscabezas@gmail.com>
2021-04-03 10:20:29 -0500

    Initialized

---
+++ 0x18-merge_sort/0-merge_sort.c
@@ -0,0 +1,13 @@
+#include "sort.h"
+
+/**
+ * merge_sort - Merge sort
+ * @array: Array to sort
+ * @size: Array size
+ * Return:
+ */
+void merge_sort(int *array, size_t size)
+{
+	(void) array;
+	(void) size;
+}
